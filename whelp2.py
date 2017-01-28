94829b255909e6410f02b7ddc5da388089df75c0
ccdipersio <ccdipersio@gmail.com>
2017-01-28 11:06:14 -0500

    test commit

--- whelp2.py
+++ whelp2.py
@@ -2,28 +2,7 @@ from TextParser import *
 from CharacterUtils import *
 from json_RoomUtils import *
 
-'''
-parser = TextParser()
-
-parser.get_new_string()
-print(parser.words)
-
-player = Player()
-player.inventory.append(1)
-player.inventory.append(2)
-
-enemy = Enemy(5)
-
-battle = BattleScene(parser, player, enemy)
-
-parser.close_files()
-
-
-parser = TextParser()
-player = Player()
-maze = Dungeon()
-maze.dungeon_control(parser, player)
-'''
+#TESTING
 
 parser = TextParser()
 player = Player()
